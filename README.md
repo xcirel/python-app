7b2b21c7dbbdd75b339c1d7e9090a74da2ddc637
Xcirel <eric@xcirel.net>
2025-05-28 00:19:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Python App
+
+A simple application to deploy on K8s
+
+```bash
+# Create a virtual environment and activate it
+python -m venv .venv
+. .venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the application
+python app.py
+```
---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
+    })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+
+    app.run()
